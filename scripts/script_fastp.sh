f6bf289b93eb6bb3a190f55cfa7b8f0641b75643
Diego Garcia <diegogsetien2@gmail.com>
2026-07-07 10:04:32 +0200

    Anotaciones en las instrucciones de Slurm

--- scripts/script_fastp.sh
+++ scripts/script_fastp.sh
@@ -2,17 +2,39 @@
 
 #Como bamos a trabajar con SLURM, tenemos que indicar las instrucciones
 
+#Con esto indicamos el nombre que tendrá la tarea en la lista de espera
 #SBATCH --job-name=bp_fastp_results_tfm
+
+#Con esto le decimos que los errores irán en la carpeta logs/ en un archivo .err
 #SBATCH --error=logs/%x-%j.err
+
+#Lo que salga bien irá a la carpeta logs/ en un archivo .out
 #SBATCH --output=logs/%x-%j.out
 
+# %x se reemplaza por el nombre del trabajo y %j por el número de ID único que Slurm le asigne a la tarea 
+
+#Las CPUs están divididas en secciones o particiones, le decimos que asigne la partición general, la estandar
 #SBATCH --partition=general
+
+#Le asignamos la prioridad al trabajo, regular es por defecto
 #SBATCH --qos=regular
+
+#Le decimos al cluster cuandos núcleos de procesador queremos
 #SBATCH --cpus-per-task=8
+
+#Le decimos que esos 8 núcleos estén físicamente en la misma máquina
 #SBATCH --nodes=1
+
+#Le decimos que solo vamos a lanzar una tarea por nodo
 #SBATCH --ntasks-per-node=1
+
+#Le decimos el límite de tiempo que queremos que tarde el script
 #SBATCH --time=03:00:00
+
+#Le decimos la memoria RAM para dedicar al trabajo, en este caso 12GB o 12000MB
 #SBATCH --mem=12000
+
+#Le decimos que cree un Job Array o matriz de tareas, para que el cluster haga 93 tareas al mismo tiempo
 #SBATCH --array=1-93%93
 
 #Creamos la carpeta para guardar los resultados de fastp
@@ -58,4 +80,4 @@ for fwd in *_1.fq.gz; do
 		--json "fastp_results/${base}_report.json" \
 		--failed_out "fastp_failed/${base}_failed_R1.fq.gz" \
 		--failed_out_R2 "fastp_failed/${base}_failed_R2.fq.gz" \
-		--thread 4
+		--thread 8
